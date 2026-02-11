f47148d23f62f64bddfd95873d13ef649e5ce0c5
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-02-11 16:51:16 +0000

    ATO-2307: add eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    ignores: ["eslint.config.ts", "node_modules", "dist"],
+  },
+);
