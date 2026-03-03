53efdcb0d4ffce7102a5cf21be7c6a2c17e1be0f
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-03-03 12:00:32 +0000

    ATO-2329: update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,6 +5,7 @@ import { includeIgnoreFile } from "@eslint/compat";
 import { fileURLToPath } from "node:url";
 import playwrightEslint from "eslint-plugin-playwright";
 import vitestEslint from "@vitest/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default defineConfig(
   includeIgnoreFile(
@@ -12,12 +13,15 @@ export default defineConfig(
     "Imported .gitignore patterns",
   ),
   eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
   {
-    files: ["**/*.ts", "**/*.js"],
+    files: ["src/**/*.ts", "src/**/*.js"],
+    extends: [
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     languageOptions: {
       ecmaVersion: "latest",
+      parser: tsParser,
       parserOptions: {
         projectService: true,
       },
@@ -27,7 +31,7 @@ export default defineConfig(
     plugins: {
       vitest: vitestEslint,
     },
-    files: ["**/*.test.ts"],
+    files: ["src/**/*.test.ts"],
     rules: {
       ...vitestEslint.configs.all.rules,
       "vitest/prefer-importing-vitest-globals": "off",
