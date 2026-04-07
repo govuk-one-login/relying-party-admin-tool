64cf2ea4486f5340bafe2d6fab90c1830742509d
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2026-04-07 14:48:07 +0100

    BAU: Set InitialNotificationStack for build
    We deployed this stack previously, however we deleted it due to
    misconfiguration. The initial deployment of this stack creates a
    service linked role that is persisted, even after the stack is
    removed.
    
    To prevent us from trying to recreate this in build, causing the stack
    to fail to deploy, we can set this to No to prevent this.

--- infra/build_notifications.tf
+++ infra/build_notifications.tf
@@ -4,7 +4,7 @@ resource "aws_cloudformation_stack" "build_notifications_stack" {
   template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/build-notifications/template.yaml"
 
   parameters = {
-    InitialNotificationStack        = "Yes"
+    InitialNotificationStack        = var.environment == "build" ? "No" : "Yes"
     BuildNotificationSlackChannelId = var.environment == "production" ? "C0AM0HM0ZRB" : "C0ALNGHKM39"
     EnrichedNotifications           = "True"
   }
