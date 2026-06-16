589ef1086d46bb1a2f2ce20f0b86db9cf41d9be2
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-06-16 10:52:58 +0100

    ATO-2541: add create a new service page

--- src/routes/services-router.ts
+++ src/routes/services-router.ts
@@ -8,4 +8,9 @@ router.get(PATH_NAMES.SERVICES, function (req, res, next) {
   res.render("services/index.njk");
 });
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+router.get(PATH_NAMES.CREATE_SERVICE, function (req, res, next) {
+  res.render("create-service/index.njk");
+});
+
 export { router as servicesRouter };
