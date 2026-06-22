b374b1bf7b829d8faafe6bc238094d0836eceacd
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-06-22 15:44:40 +0100

    ATO-2643: add service description to 'Create a new service' page

--- src/routes/services-router.ts
+++ src/routes/services-router.ts
@@ -2,7 +2,7 @@
 
 import express from "express";
 import { PATH_NAMES } from "../app.constants.js";
-import { validateServiceNameRequest } from "../components/create-service/create-service-validation.js";
+import { validateServiceRequest } from "../components/create-service/create-service-validation.js";
 import { createServicePost } from "../components/create-service/create-service-controller.js";
 
 const router = express.Router();
@@ -17,7 +17,7 @@ router.get(PATH_NAMES.CREATE_SERVICE, function (req, res, next) {
 
 router.post(
   PATH_NAMES.CREATE_SERVICE,
-  validateServiceNameRequest(),
+  validateServiceRequest(),
   createServicePost()
 );
 
