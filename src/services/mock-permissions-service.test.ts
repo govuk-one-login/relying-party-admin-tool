e61fae3a085cf4650e3928311bff5e0167041421
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-06-23 11:29:13 +0100

    BAU: add a permissions enum and use in the permissions service

--- src/services/mock-permissions-service.test.ts
+++ src/services/mock-permissions-service.test.ts
@@ -1,7 +1,61 @@
-import { MockPermissionsService } from "./mock-permissions-service.js";
+import { UserPermission } from "../models/permissions.js";
+import { ClientEnvironment } from "../types.js";
+import { mockPermissionsService } from "./mock-permissions-service.js";
 
-describe("MockPermissionsService", () => {
-  it("check should always return true", () => {
-    expect(MockPermissionsService.check("", "", "")).toBe(true);
+describe("mockPermissionsService", () => {
+  describe("check", () => {
+    it("check should return true for READER permissions", () => {
+      expect(mockPermissionsService.check("", UserPermission.READER, "")).toBe(
+        true
+      );
+    });
+
+    it("check should return true for WRITER_INT permissions", () => {
+      expect(
+        mockPermissionsService.check("", UserPermission.WRITER_INT, "")
+      ).toBe(true);
+    });
+
+    it("check should return true for WRITER_PROD permissions", () => {
+      expect(
+        mockPermissionsService.check("", UserPermission.WRITER_PROD, "")
+      ).toBe(true);
+    });
+
+    it("check should return true for MANAGER permissions", () => {
+      expect(mockPermissionsService.check("", UserPermission.MANAGER, "")).toBe(
+        true
+      );
+    });
+  });
+
+  describe("checkUserHasReaderPermissions", () => {
+    it("check should return true for any user", () => {
+      expect(mockPermissionsService.checkUserHasReaderPermissions("", "")).toBe(
+        true
+      );
+    });
+  });
+
+  describe("checkUserHasWriterPermissions", () => {
+    it("check should return true for any user in integration", () => {
+      expect(
+        mockPermissionsService.checkUserHasWriterPermissions(
+          "",
+          "",
+          ClientEnvironment.INTEGRATION
+        )
+      ).toBe(true);
+    });
+
+    it("check should return true for any user in production", () => {
+      expect(
+        mockPermissionsService.checkUserHasWriterPermissions(
+          "",
+          "",
+          ClientEnvironment.PRODUCTION
+        )
+      ).toBe(true);
+    });
   });
 });
