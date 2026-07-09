f0b21ddd71e208fec182b31c0fefb394f109ceb1
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-07-09 12:07:48 +0100

    BAU: remove console log from Enter Client Name validation

--- src/components/create-client/enter-client-name/enter-client-name-validation.ts
+++ src/components/create-client/enter-client-name/enter-client-name-validation.ts
@@ -3,13 +3,6 @@ import { ValidationChainFunc } from "../../../types.js";
 import { validateBodyMiddleware } from "../../../middleware/form-validation-middleware.js";
 
 export function validateEnterClientNameRequest(): ValidationChainFunc {
-  const tmp = validateClientName({
-    required: "Enter your client name",
-    maxLength: "Your client name must be less than 255 characters long",
-    asciiOnly: "Your client name must only use ASCII characters",
-    startsWithColon: "Your client name cannot start with ':'",
-  });
-  console.log(tmp);
   return [
     validateClientName({
       required: "Enter your client name",
