b0de1e3b70011a813827b3fcee0a4e03e9f41f7f
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2026-07-30 09:56:57 +0100

    ATO-2630: add integration test for getting services in user permission table

--- integration-tests/user-permissions-data-store.test.ts
+++ integration-tests/user-permissions-data-store.test.ts
@@ -1,6 +1,11 @@
 import { integrationTest, setupUserPermissionsTable } from "./base.js";
 import { User } from "../src/models/user.js";
-import { getUser } from "../src/datastores/user-permissions-data-store.js";
+import {
+  getServicesWithRelationForUser,
+  getUser,
+} from "../src/datastores/user-permissions-data-store.js";
+import { Relation } from "../src/models/relation.js";
+import { UserPermission } from "../src/models/permissions.js";
 
 describe("user permissions data store tests", () => {
   setupUserPermissionsTable();
@@ -26,6 +31,33 @@ describe("user permissions data store tests", () => {
       const user = await getUser("not-a-user-id");
 
       expect(user).toBeUndefined();
+    }
+  );
+
+  integrationTest(
+    "should get services with relation from table by ID if user exists",
+    async ({ addUserRelationsToDynamo }) => {
+      const relationServiceId1 = "1";
+      const existingRelation1: Relation = {
+        userId: "test-user-id",
+        object: `service:${relationServiceId1}`,
+        relation: UserPermission.READER,
+      };
+      const relationServiceId2 = "2";
+      const existingRelation2: Relation = {
+        userId: "test-user-id",
+        object: `service:${relationServiceId2}`,
+        relation: UserPermission.READER,
+      };
+      await addUserRelationsToDynamo(existingRelation1);
+      await addUserRelationsToDynamo(existingRelation2);
+
+      const relation = await getServicesWithRelationForUser(
+        "test-user-id",
+        UserPermission.READER
+      );
+
+      expect(relation).toStrictEqual([relationServiceId1, relationServiceId2]);
     }
   );
 });
