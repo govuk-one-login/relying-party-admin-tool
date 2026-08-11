e71b39a0919e12149dacc50120cbbdec7bf069ef
Isaac Au <isaac.au@digital.cabinet-office.gov.uk>
2026-08-11 14:25:32 +0100

    ATO-2631: add service card information  - add a services contoller  - pass through services to view all services page    - serviceId to be able to link to service page    - serviceName on the servicess page

---
+++ src/components/services/services-controller.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express";
+import { ExpressRouteFunc } from "../../types.js";
+import { getServicesUserCanView } from "../../services/service-permissions-service.js";
+
+export const servicesGet = (): ExpressRouteFunc => {
+  return async (req: Request, res: Response): Promise<void> => {
+    const services = await getServicesUserCanView("user");
+
+    return res.render("service/index.njk", {
+      services: services,
+    });
+  };
+};
