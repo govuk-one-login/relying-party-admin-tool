5648cd89d3bd355527c63c0353ac189df1e04aa9
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-08-20 12:18:24 +0100

    ATO-2762: add new validator to create service page

--- src/components/create-service/create-service-validation.ts
+++ src/components/create-service/create-service-validation.ts
@@ -1,29 +1,12 @@
-import { body, ValidationChain } from "express-validator";
 import { ValidationChainFunc } from "../../types.js";
-import { validateBodyMiddleware } from "../../middleware/form-validation-middleware.js";
+import { validateFieldsMiddleware } from "../../middleware/form-validation-middleware.js";
+import { enterServiceNameFieldValidator } from "../../validation/service-validators.js";
 
 export const validateServiceRequest = (): ValidationChainFunc => {
   return [
-    validateServiceName({
-      required: "Enter your service name",
-      asciiOnly: "Your service name must only use ASCII characters",
-      maxLength: "Your service name must be less than 256 characters long",
-    }),
-    validateBodyMiddleware("create-service/index.njk"),
+    validateFieldsMiddleware(
+      "create-service/index.njk",
+      enterServiceNameFieldValidator
+    ),
   ];
-};
-
-const validateServiceName = (validationMessages: {
-  required: string;
-  asciiOnly: string;
-  maxLength: string;
-}): ValidationChain => {
-  return body("name")
-    .trim()
-    .notEmpty()
-    .withMessage(validationMessages.required)
-    .isAscii()
-    .withMessage(validationMessages.asciiOnly)
-    .isLength({ max: 256 })
-    .withMessage(validationMessages.maxLength);
 };
