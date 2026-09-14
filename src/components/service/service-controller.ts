b82ad6eda14ab7b3a907823dffc2a67e2791878e
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-09-14 12:08:11 +0100

    ATO-2901: remove checkUserHasReaderPermissions from page GET controllers

--- src/components/service/service-controller.ts
+++ src/components/service/service-controller.ts
@@ -10,57 +10,51 @@ import { getClientsByServiceId } from "../../datastores/services-data-store.js";
 export const serviceGet = (): ExpressRouteFunc => {
   return async (req: Request, res: Response): Promise<void> => {
     const serviceId = req.params.serviceId as string;
-    if (
-      await permissionsService.checkUserHasReaderPermissions("user", serviceId)
-    ) {
-      const hasIntegrationWriterPermissions: boolean =
-        await permissionsService.checkUserHasWriterPermissions(
-          "user",
-          serviceId,
-          ClientEnvironment.INTEGRATION
-        );
-      const hasManagerPermissions =
-        await permissionsService.checkUserHasManagerPermissions(
-          "user",
-          serviceId
-        );
+    const hasIntegrationWriterPermissions: boolean =
+      await permissionsService.checkUserHasWriterPermissions(
+        "user",
+        serviceId,
+        ClientEnvironment.INTEGRATION
+      );
+    const hasManagerPermissions =
+      await permissionsService.checkUserHasManagerPermissions(
+        "user",
+        serviceId
+      );
 
-      const sideNavItems = [
-        {
-          active: true,
-          text: "Clients",
-          href: req.path,
-        },
-        {
-          active: false,
-          text: "Team members",
-          href: path.posix.join(req.path, "team-members"),
-        },
-      ];
-      try {
-        const service = await getServiceByServiceId(serviceId);
+    const sideNavItems = [
+      {
+        active: true,
+        text: "Clients",
+        href: req.path,
+      },
+      {
+        active: false,
+        text: "Team members",
+        href: path.posix.join(req.path, "team-members"),
+      },
+    ];
+    try {
+      const service = await getServiceByServiceId(serviceId);
 
-        const allClients = await getClientsByServiceId(serviceId);
+      const allClients = await getClientsByServiceId(serviceId);
 
-        const productionClient = allClients.filter(
-          (client) => client.env === "production"
-        )[0];
-        const integrationClients = allClients.filter(
-          (client) => client.env === "integration"
-        );
+      const productionClient = allClients.filter(
+        (client) => client.env === "production"
+      )[0];
+      const integrationClients = allClients.filter(
+        (client) => client.env === "integration"
+      );
 
-        return res.render("service/index.njk", {
-          hasIntegrationWriterPermissions,
-          ...(hasManagerPermissions && { sideNavItems }),
-          service,
-          productionClient,
-          ...(integrationClients.length > 0 && { integrationClients }),
-        });
-      } catch {
-        res.redirect(PATH_NAMES["500_ERROR"]);
-      }
-    } else {
-      return res.redirect(PATH_NAMES.ROOT);
+      return res.render("service/index.njk", {
+        hasIntegrationWriterPermissions,
+        ...(hasManagerPermissions && { sideNavItems }),
+        service,
+        productionClient,
+        ...(integrationClients.length > 0 && { integrationClients }),
+      });
+    } catch {
+      res.redirect(PATH_NAMES["500_ERROR"]);
     }
   };
 };
