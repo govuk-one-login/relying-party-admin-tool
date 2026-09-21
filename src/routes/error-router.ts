07c4af8a8c4735dc925d7decd6c324049b0e8993
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-09-21 14:34:57 +0100

    BAU: add missing status codes to the error pages

--- src/routes/error-router.ts
+++ src/routes/error-router.ts
@@ -4,14 +4,17 @@ import { PATH_NAMES } from "../app.constants.js";
 const router = express.Router();
 
 router.get(PATH_NAMES["500_ERROR"], (_req, res) => {
+  res.status(500);
   res.render("common/errors/500.njk");
 });
 
 router.get(PATH_NAMES["403_ERROR"], (_req, res) => {
+  res.status(403);
   res.render("common/errors/403.njk");
 });
 
 router.all("/{*any}", (_req, res) => {
+  res.status(404);
   res.render("common/errors/404.njk");
 });
 
